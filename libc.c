68bd6eb8134a02755a2cceb4ae4d2ad6f060b3c7
Bmiro <bartomeumiro@gmail.com>
2011-09-20 16:51:36 +0200

    Intentant programar primera syscall

--- libc.c
+++ libc.c
@@ -1,12 +1,23 @@
+
 /*
  * libc.c 
  */
 
 #include <libc.h>
 
-/* Wrapper of  write system call*/
-int write(int fd,char *buffer,int size)
-{
-	return 0;
+/* Wrapper of write system call*/
+int write(int fd, char *buffer, int size) {
+  
+  /*TODO*/
+  __asm__ __volatile__(
+    "popl %edx\n"
+    "popl %ecx\n"
+    "popl %ebx\n"
+    "movl $4, %eax\n" 
+    "int $0x80\n");
+    
+    /* Errno */
+    
+    
 }
 
