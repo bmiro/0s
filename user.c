ea2b19ceb7dbf378bc44ae39eb9ee932a918c3f3
Bmiro <bartomeumiro@gmail.com>
2011-09-21 14:34:40 +0200

    Introduint jocs de proves

--- user.c
+++ user.c
@@ -1,16 +1,15 @@
 #include <libc.h>
 
 int __attribute__ ((__section__(".text.main")))
-  main(void)
-{
+  main(void) {
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
     /* Uncomment next line to call the initial routine for the test cases */
     /* runjp(); */
 
   //__asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); /* Generates a General protection Exception */
-
-  write(1, "ABCDEFGHIJKLMNOPQRST", 20);
+  
+  runjp();
   
   while(1);
 }
