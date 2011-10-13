f2562d19e144177b00f08422843b6bb215d2863e
bmiro <bartomeumiro@gmail.com>
2011-10-13 12:46:35 +0200

    Dedicatoria Dennis Ritchie

--- user.c
+++ user.c
@@ -2,18 +2,10 @@
 
 int __attribute__ ((__section__(".text.main"))) main(void) {
   int pid;
-  
-//   write(1, "AA\n", 3);
-//   write(1, "BB\n", 3);
-//   write(1, "CC\n", 3);
-//   write(1, "DD\n", 3);
-//   write(1, "EE\n", 3);
-//   write(1, "FF\n", 3);
-//   write(1, "GG\n", 3);
-//   write(1, "HH\n", 3);	  
-//   write(1, "II\n", 3);
-//   write(1, "JJ\n", 3);
-//   write(1, "KK", 3);
+  write(1, "Aquesta entrega esta dedicada a Dennis Ritchie 1941 - 9 d'Octubre 2011 per la \
+seva contribucio al llenguatge C i sistemes Unix, intimaments lligats a aquesta \
+assignatura.\n", 173);
+
 //  runjp();
 
   while(1);
