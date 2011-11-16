ef15b8b6c89113072125afcfa7e0d0bc5d8b1ce0
bmiro <bartomeumiro@gmail.com>
2011-11-16 10:38:25 +0100

    Començant amb el buffer circular

--- circbuff.c
+++ circbuff.c
@@ -1,1 +1,21 @@
 #include <circbuff.h>
+
+char circbuff[CIRC_BUFF_SIZE];
+int head, tail;
+
+int init_circ_buff() {
+  head = 0;
+  tail = 1;
+}
+
+/* Returns in buffer the amout of charactars specified in size,
+   also returns the number of characters read. -1 if error */
+int get_character(char *buffer, int size) {
+  
+}
+
+/* Puts the character c into the buffer */
+int set_character(char c) {
+  
+}
+
--- include/circbuff.h
+++ include/circbuff.h
@@ -1,4 +1,15 @@
 #ifndef __CIRCBUFF_H__
 #define __CIRCBUFF_H__
 
+#define CIRC_BUFF_SIZE 80
+
+int init_circ_buff();
+
+/* Returns in buffer the amout of charactars specified in size,
+   also returns the number of characters read. -1 if error */
+int get_character(char *buffer, int size);
+
+/* Puts the character c into the buffer */
+int set_character(char c);
+
 #endif
