70d9b14763ee8b846d10a9277cd5a91b5ddf28e5
Bmiro <bartomeumiro@gmail.com>
2011-11-18 18:24:13 +0100

    Codi d'usuari de prova

--- user.c
+++ user.c
@@ -2,7 +2,7 @@
 
 int __attribute__ ((__section__(".text.main"))) main(void) {
 
-  char c = 'Q';
+  char c = 'C';
   char d = 'D';
   int p;
   
@@ -14,28 +14,40 @@ int __attribute__ ((__section__(".text.main"))) main(void) {
 pare dels sistemes Unix i llenguatge C, fonament d'aquesta assignatura.", 145);
   //while(1);
   
-  p = fork();
-  
-  if (p == 0) {
-    while(1) {
-      read(0, &c, 1);
-      write(1, &c, 1);
-    }
-  }
-  
-  while(1) {
-    //write(1, "FATHER", 6);
-  }
+//   p = fork();
+//   
+//   if (p == 0) {
+//     while(1) {
+//       read(0, &c, 1);
+//       write(1, &c, 1);
+//     }
+//   }
+//   
+//   while(1) {
+//     //write(1, "FATHER", 6);
+//   }
 
   f = open("fitxer", O_CREAT|O_WRONLY);
 
-  read(STDIN, &c, 1);
+  write(STDOUT, "OPEN_DONE\n", 10);
+  
+  //read(STDIN, &c, 1);
   write(f, &c, 1);
+  
+  write(STDOUT, "WRITE_DONE\n", 11);
   
   close(f);
   
+  write(STDOUT, "CLOSE_DONE\n", 12);
+
   f = open("fitxer", O_CREAT|O_RDONLY);
+  
+  write(STDOUT, "OPEN_DONE\n", 10);
+  
   read(f, &d, 1);
+  
+  write(STDOUT, "READ_DONE\n", 10);
+
   write(STDOUT, &d, 1);
   
   close(f);
